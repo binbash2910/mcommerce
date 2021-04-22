fd1d3a449bf95943d7402e7b4d12372a08315462
Yannick MIMBE <lucienyannick@gmail.com>
2021-04-22 11:02:14 +0200

    change pom

--- microservice-paiement/src/test/java/com/mpaiement/MpaiementApplicationTests.java
+++ microservice-paiement/src/test/java/com/mpaiement/MpaiementApplicationTests.java
@@ -1,9 +1,13 @@
 package com.mpaiement;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
--- microservice-produits/src/test/java/com/mproduits/MproduitsApplicationTests.java
+++ microservice-produits/src/test/java/com/mproduits/MproduitsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mproduits;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
